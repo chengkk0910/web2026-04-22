09b9189d7a34ba9c8151453a7b75a6d3432d7f0e
kai <cgk0910@gmail.com>
2026-08-26 09:23:02 +0800

    obj load json to table

---
+++ s20260826/data/data.js
@@ -0,0 +1,18 @@
+let data123 = [
+    {
+        'id': '1',
+        'name': 'amy'
+    },
+    {
+        'id': '2',
+        'name': 'bob'
+    },
+    {
+        'id': '3',
+        'name': 'cat'
+    },
+    {
+        'id': '4',
+        'name': 'dog'
+    }
+];
